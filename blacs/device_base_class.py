300fe2533c626d76328b8f0e8f7faf4ddf515a1b
Shafin <haqueshafinul@gmail.com>
2024-06-30 09:54:18 -0400

    final values of run are being saved in post_exp state function

--- blacs/device_base_class.py
+++ blacs/device_base_class.py
@@ -764,6 +764,18 @@ class DeviceTab(Tab):
         
         if success:
             if skip_manual:
+                # Update the channel with the final values of the run, but no need to 
+                # grab the QTlock and update the GUI.
+                for channel, value in self._final_values.items():
+                    if channel in self._AO:
+                        self._AO[channel].set_value(value,program=False,update_gui=False)
+                    elif channel in self._DO:
+                        self._DO[channel].set_value(value,program=False,update_gui=False)
+                    elif channel in self._image:
+                        self._image[channel].set_value(value,program=False,update_gui=False)
+                    elif channel in self._DDS:
+                        self._DDS[channel].set_value(value,program=False,update_gui=False)
+
                 # Do not transition_to_manual, continue state machine flow from
                 # the MODE_POST_EXP state
                 notify_queue.put([self.device_name,'success'])
